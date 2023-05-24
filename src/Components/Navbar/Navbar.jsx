cddb8c267d06843c59cb6cd2dd507ca52a71b1ac
MohammadHalaweh7 <s11940976@stu.najah.edu>
2023-05-24 19:00:18 +0300

    Add Cart Modal

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,9 @@ import style from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav className = {` navbar navbar-expand-lg bg-body-tertiary ${style.container} `} >
+    <nav
+      className={` navbar navbar-expand-lg bg-body-tertiary ${style.container} `}
+    >
       <div className="container-fluid">
         <a className={`navbar-brand ${style.title}`} href="#">
           Shopping List
@@ -20,7 +22,6 @@ export default function Navbar() {
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
@@ -54,15 +55,64 @@ export default function Navbar() {
           </ul>
 
           <div className={`d-flex ${style.icons}`}>
-            <div>
+            <div data-bs-toggle="modal" data-bs-target="#exampleModal">
               <i class="fa-solid fa-cart-shopping"></i>
+              <div className={`${style["notification-cart"]}`}>9</div>
             </div>
 
             <div>
               <i className="fa-solid fa-gear"></i>
             </div>
           </div>
-          
+        </div>
+      </div>
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Image</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Count</th>
+                  </tr>
+                </thead>
+                <tbody></tbody>
+              </table>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
